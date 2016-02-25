3424bd8a34f435777eb37cddd2d1f3cb263f2995
Bugro <guillaume.burgaud@free.fr>
2016-02-25 11:50:07 +0100

    Le server et le client fonctionnent

--- raspConnect/client.py
+++ raspConnect/client.py
@@ -1,23 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 25 10:27:26 2016
+
+@author: guill
+"""
+
 
 import socket
 import datetime
 import time
+import RPi.GPIO as io
+io.setmode(io.BCM)
+
+in1_pin = 18
+in2_pin = 19
+
+io.setup(in1_pin, io.OUT)
+io.setup(in2_pin, io.OUT)
+
+p1 = io.PWM(in1_pin, 4000)
+p1.start(0)
+p2 = io.PWM(in2_pin, 4000)
+p2.start(100)
+
+pause_time = 0.02
 
 s = socket.socket()
-host = '169.254.21.120'# ip of the computer
-port = 1300
+host = '169.254.21.120'# ip of bugro's PC
+#host = '169.254.179.113'# ip of robot's raspberry
+port = 1100
 try:
-        s.connect((host,port))
+    s.connect((host,port))
 except:
-        print("Unable to connect to server.\nCheck if IP address is correct and your firewalirewall options.")
+    print("Unable to connect to server.\nCheck if IP address is correct and your firewall")
 
 
 try:
-    while(1):
-        date = str(datetime.datetime.now())
-        s.sendall(date)
-        print(s.recv(1024))
-        time.sleep(1)
+        print s.recv(1024)
+        s.sendall("OK")
+        while True:
+                data =  int(s.recv(1024))
+                print data
+                p1.ChangeDutyCycle(float(data))
+                p2.ChangeDutyCycle(float(data))
+                time.sleep(1)
 except:
+        p1.stop()            # stop the white PWM output
+        p2.stop()              # stop the red PWM output
+        io.cleanup()          # clean up GPIO on CTRL+C exit
         print("\nYou've been disconnected")
         s.close()
+
+
--- raspConnect/server.py
+++ raspConnect/server.py
@@ -1,24 +1,8 @@
-## Echo server program
-#import socket
-#
-#HOST = ''                 # Symbolic name meaning all available interfaces
-#PORT = 22              # Arbitrary non-privileged port
-#s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#s.bind((HOST, PORT))
-#s.listen(1)
-#conn, addr = s.accept()
-#print 'Connected by', addr
-#while 1:
-#    data = conn.recv(1024)
-#    if not data: break
-#    conn.sendall(data+"coucou")
-#conn.close()
-
 import socket
 
 s = socket.socket()
 host = '' #Symbolic name meaning all available interfaces
-port = 1300
+port = 1100
 s.bind((host, port))
 
 s.listen(5)
@@ -27,12 +11,13 @@ while True:
     c, addr = s.accept()
     print ('Got connection from',addr)
     c.send('Thanks for connecting\n')
+    synchro = c.recv(1024)
+    print(synchro)
     try:
         while True:
-            data = c.recv(1024)
-            print(data)
-            if not data: break
-            c.send("It is "+data)
+            
+            data = str(raw_input("Vitesse"))
+            c.send(data)
     except:
         print("No connexion from client")
         c.close()
