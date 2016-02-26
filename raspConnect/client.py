d041625366186956a02a4ca51813468cf0f80d9a
Bugro <guillaume.burgaud@free.fr>
2016-02-26 14:10:35 +0100

    Pilotage au clavier
    A partir d'entrées clavier on pilote le robot (ZQSD).
    Si on utilise Windows on peut décommenter la ligne getche() et commenter
    celle d'input() pour ne plus avoir à appuyer sur Enter en permanence

--- raspConnect/client.py
+++ raspConnect/client.py
@@ -18,7 +18,7 @@ in2_pin = 19
 io.setup(in1_pin, io.OUT)
 io.setup(in2_pin, io.OUT)
 
-p1 = io.PWM(in1_pin, 4000)
+p1 = io.PWM(in1_pin, 4000) #Va savoir pourquoi cela ne fonctionne pas à 20kHz
 p1.start(0)
 p2 = io.PWM(in2_pin, 4000)
 p2.start(100)
@@ -28,7 +28,7 @@ pause_time = 0.02
 s = socket.socket()
 host = '169.254.21.120'# ip of bugro's PC
 #host = '169.254.179.113'# ip of robot's raspberry
-port = 1100
+port = 1300
 try:
     s.connect((host,port))
 except:
@@ -36,19 +36,24 @@ except:
 
 
 try:
-        print s.recv(1024)
-        s.sendall("OK")
+        print(str(s.recv(1024)))
         while True:
-                data =  int(s.recv(1024))
-                print data
-                p1.ChangeDutyCycle(float(data))
-                p2.ChangeDutyCycle(float(data))
-                time.sleep(1)
+            data = str(s.recv(1024))
+            if(data[0] == "R"):
+                dataSplited = data.split("=")
+                dataR = dataSplited[1].split("L")[0]
+                dataL = dataSplited[2]
+                print dataR
+                print dataL
+                dataR = int(dataR)
+                dataL = int(dataL)
+                p1.ChangeDutyCycle(float(dataR))
+                p2.ChangeDutyCycle(float(dataL))
+            else:
+                print "Command was lost"
 except:
         p1.stop()            # stop the white PWM output
         p2.stop()              # stop the red PWM output
         io.cleanup()          # clean up GPIO on CTRL+C exit
         print("\nYou've been disconnected")
         s.close()
-
-
--- raspConnect/server.py
+++ raspConnect/server.py
@@ -1,8 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 26 10:00:52 2016
+
+@author: guill
+"""
+
 import socket
 
 s = socket.socket()
 host = '' #Symbolic name meaning all available interfaces
-port = 1100
+port = 1300
 s.bind((host, port))
 
 s.listen(5)
@@ -11,13 +18,22 @@ while True:
     c, addr = s.accept()
     print ('Got connection from',addr)
     c.send('Thanks for connecting\n')
-    synchro = c.recv(1024)
-    print(synchro)
     try:
         while True:
-            
-            data = str(raw_input("Vitesse"))
-            c.send(data)
+            cmd = str(raw_input())
+#            cmd = msvcrt.getche() #Commande pour Windows permettant de ne pas appuyer sur Enter
+            if cmd == 'z':
+                c.send("R=100")
+                c.send("L=100")
+            elif cmd == 'd':
+                c.send("R=-100")
+                c.send("L=100")
+            elif cmd == 'q':
+                c.send("R=100")
+                c.send("L=-100")
+            else:
+                c.send("R=50")
+                c.send("L=50")
     except:
         print("No connexion from client")
         c.close()
